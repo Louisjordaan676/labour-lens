442423cc1e0514cad09102043081a1fa46b37e08
Louisjordaan676 <jordaanlouis4@gmail.com>
2026-08-21 15:43:34 +0200

    basic chatbot with memory used RunnableWithMessageHistory"

--- src/labour_lens/chatbot.py
+++ src/labour_lens/chatbot.py
@@ -1,10 +1,23 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 load_dotenv()
 
+chat_history = InMemoryChatMessageHistory()
+
+
+def get_session_history(session_id):
+    return chat_history
+
 
 llm = ChatOpenAI(model="gpt-4o-mini")
+
+chatbot = RunnableWithMessageHistory(
+    llm,
+    get_session_history
+)
 
 while True:
 
@@ -14,6 +27,8 @@ while True:
         print("goodbye, chat to you soon.")
         break
 
-    response = llm.invoke(user_input)
+    response = chatbot.invoke(user_input,
+                              config={"configurable": {"session_id":
+                                                       "conversation_1"}})
 
     print(response.content)
