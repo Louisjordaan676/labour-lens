ed71f0c5ea8580d70d4a43bb4c8135c35663a3bc
Louisjordaan676 <jordaanlouis4@gmail.com>
2026-08-28 13:10:36 +0200

    Update doc_processor.py

--- src/labour_lens/doc_processor.py
+++ src/labour_lens/doc_processor.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
 
 
 # load the document
@@ -16,10 +17,16 @@ chunks = r_text_splitter.split_documents(pages)
 
 embeddings_model = OpenAIEmbeddings(model="text-embedding-3-small")
 
-embed_exp = embeddings_model.embed_query(chunks[0].page_content)
+# embed_exp = embeddings_model.embed_query(chunks[0].page_content)
 
-new_chunks = [item.page_content for item in chunks]
+# new_chunks = [item.page_content for item in chunks]
 
-result = embeddings_model.embed_documents(new_chunks)
-# print(len(result)) # 197 because thats how many chunks are in "chunks"
-# print(len(result[0])) # 1536 , every string has this amount of vectors
+# result = embeddings_model.embed_documents(new_chunks)
+# # print(len(result)) # 197 because thats how many chunks are in "chunks"
+# # print(len(result[0])) # 1536 , every string has this amount of vectors
+
+vector_store = Chroma(
+    collection_name="basic_conditions_of_employment",
+    embedding_function=embeddings_model,
+    persist_directory="./chroma_langchain_dh"
+)
